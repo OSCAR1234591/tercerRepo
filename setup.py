9834830358072c238dad9fff638973da63c98b5f
Oscar <oscar.ubiera@atmira.com>
2026-03-03 16:54:00 +0100

    lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Osacr Ubiera",                         # Tu nombre
+    author_email="oscar.ubiera@platzi.com",                 # Tu correo electrónico
+    url="https://github.com/OSCAR1234591/tercerRepo",     # URL del proyecto
+)
